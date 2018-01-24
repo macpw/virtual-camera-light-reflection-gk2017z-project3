918b8692d86d4df4137838e4ad47331c9d56e37f
pawel <pawel@omega>
2018-01-24 20:06:23 +0100

    First commit.

---
+++ src/main/java/com/mycompany/virtual_camera_light_reflection/App.java
@@ -0,0 +1,11 @@
+package com.mycompany.virtual_camera_light_reflection;
+
+/**
+ *
+ * @author Paweł Mac
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello App ;]");
+    }
+}
